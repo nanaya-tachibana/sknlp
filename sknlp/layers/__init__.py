70cb65a9e8d5d4178c765be63838e99259b7f82f
nanaya <nanaya100@gmail.com>
2021-07-28 21:03:28 +0800

    remove multi_lstm layer.

--- sknlp/layers/__init__.py
+++ sknlp/layers/__init__.py
@@ -1,6 +1,5 @@
 from .mlp import MLPLayer
 from .lstmp import LSTMPCell, LSTMP
-from .multi_lstmp import MultiLSTMP
 from .bert_tokenization import BertTokenizationLayer
 from .bert_layer import (
     BertEncodeLayer,
@@ -16,7 +15,6 @@ __all__ = [
     "MLPLayer",
     "LSTMPCell",
     "LSTMP",
-    "MultiLSTMP",
     "BertTokenizationLayer",
     "BertEncodeLayer",
     "BertPreprocessingLayer",
