3fa7f566c14c926d16bcf2bd7416d74123c53a0e
nanaya <nanaya100@gmail.com>
2021-10-14 17:06:30 +0800

    suppress logging while testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import os
 import logging
-from absl import logging as absl_logging
 
-logging.getLogger("tensorflow").setLevel(logging.ERROR)
-logging.getLogger("sknlp").setLevel(logging.ERROR)
+from tensorflow import get_logger
+from sknlp.utils.logging import logger
+
+get_logger().setLevel(logging.ERROR)
+logger.setLevel(logging.ERROR)
 os.environ["KMP_AFFINITY"] = "noverbose"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-absl_logging.set_verbosity(logging.ERROR)
