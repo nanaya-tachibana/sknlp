42980b5c799d5a8f4af26ed5a167fe611cbb5559
nanaya <nanaya100@gmail.com>
2021-10-20 16:05:18 +0800

    change test name.

--- tests/module/generators/test_bert_generator.py
+++ tests/module/generators/test_bert_generator.py
@@ -9,8 +9,6 @@ from sknlp.module.generators import BertGenerator
         pytest.param(False, id="file_data"),
     ],
 )
-def test_bert_classifier(
-    use_raw_data, model_common_test, raw_data, file_data, bert2vec
-):
+def test_bert_generator(use_raw_data, model_common_test, raw_data, file_data, bert2vec):
     model = BertGenerator(dropout=0.1, text2vec=bert2vec)
     model_common_test(BertGenerator, model, raw_data, file_data, use_raw_data, 1e-4)
