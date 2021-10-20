2a794a3a94e97bbe6c6ae238797df7aabe806186
nanaya <nanaya100@gmail.com>
2021-10-20 17:36:35 +0800

    change dropout rate.

--- tests/module/generators/test_bert_generator.py
+++ tests/module/generators/test_bert_generator.py
@@ -10,5 +10,5 @@ from sknlp.module.generators import BertGenerator
     ],
 )
 def test_bert_generator(use_raw_data, model_common_test, raw_data, file_data, bert2vec):
-    model = BertGenerator(dropout=0.1, text2vec=bert2vec)
+    model = BertGenerator(dropout=0.0, text2vec=bert2vec)
     model_common_test(BertGenerator, model, raw_data, file_data, use_raw_data, 1e-4)
