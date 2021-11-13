204899153e137b4cfee0ea29dfc8296652060f7c
nanaya <nanaya100@gmail.com>
2021-11-13 15:06:17 +0800

    feat(data): retrieval任务添加evaluation dataset

--- sknlp/data/__init__.py
+++ sknlp/data/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from .classification_dataset import ClassificationDataset, BertClassificationDataset
 from .tagging_dataset import TaggingDataset, BertTaggingDataset
-from .retrieval_dataset import RetrievalDataset, BertRetrievalDataset
+from .retrieval_dataset import (
+    RetrievalDataset,
+    BertRetrievalDataset,
+    RetrievalEvaluationDataset,
+    BertRetrievalEvaluationDataset,
+)
 from .generation_dataset import GenerationDataset, BertGenerationDataset
 from .nlp_dataset import NLPDataset
 
@@ -12,6 +17,8 @@ __all__ = [
     "TaggingDataset",
     "GenerationDataset",
     "RetrievalDataset",
+    "RetrievalEvaluationDataset",
+    "BertRetrievalEvaluationDataset",
     "BertClassificationDataset",
     "BertTaggingDataset",
     "BertGenerationDataset",
--- sknlp/data/retrieval_dataset.py
+++ sknlp/data/retrieval_dataset.py
@@ -17,7 +17,7 @@ class RetrievalDataset(NLPDataset):
     def __init__(
         self,
         vocab: Vocab,
-        labels: Sequence[str],
+        labels: Sequence[int],
         segmenter: Optional[str] = None,
         X: Optional[Sequence[Any]] = None,
         y: Optional[Sequence[Any]] = None,
@@ -27,6 +27,7 @@ class RetrievalDataset(NLPDataset):
         max_length: Optional[int] = None,
         text_dtype: tf.DType = tf.int64,
         label_dtype: tf.DType = tf.int64,
+        column_dtypes=["str", "str", "str"],
         **kwargs,
     ):
         super().__init__(
@@ -39,25 +40,35 @@ class RetrievalDataset(NLPDataset):
             has_label=has_label,
             max_length=max_length,
             na_value="",
-            column_dtypes=["str", "str", "str"],
+            column_dtypes=column_dtypes,
             text_dtype=text_dtype,
             label_dtype=label_dtype,
         )
 
     @property
-    def y(self) -> list[float]:
-        return []
+    def X(self) -> list[Any]:
+        return [
+            data[0].decode("UTF-8")
+            for data in self._original_dataset.as_numpy_iterator()
+        ]
+
+    @property
+    def y(self) -> list[str]:
+        if not self.has_label:
+            return []
+        return [
+            [d.decode("UTF-8") for d in data[1:]]
+            for data in self._original_dataset.as_numpy_iterator()
+        ]
 
     @property
     def batch_padding_shapes(self) -> tuple:
         return (None, None)
 
     def _format_y(self, y: Sequence[str | Sequence[str]]) -> list[Sequence[str]]:
-        if isinstance(y[0], str):
-            y = [y]
-        return y
+        return self._format_X(y)
 
-    def py_transform(self, *data: Sequence[tf.Tensor]) -> list[Any]:
+    def py_transform(self, *data: Sequence[tf.Tensor]) -> np.ndarray:
         token_ids_list = self.vocab.token2idx(self.py_text_transform(data))
         return tf.keras.preprocessing.sequence.pad_sequences(
             token_ids_list, padding="post"
@@ -90,6 +101,101 @@ class RetrievalDataset(NLPDataset):
         )
 
 
+def _reshape_x(*data):
+    x = data[0]
+    reshaped_x = tf.reshape(x, (-1, tf.shape(x)[-1]))
+    if len(data) == 1:
+        return [reshaped_x]
+    else:
+        return [reshaped_x, data[-1]]
+
+
+class RetrievalEvaluationDataset(RetrievalDataset):
+    def __init__(
+        self,
+        vocab: Vocab,
+        labels: Sequence[int],
+        segmenter: Optional[str] = None,
+        X: Optional[Sequence[Any]] = None,
+        y: Optional[Sequence[Any]] = None,
+        csv_file: Optional[str] = None,
+        in_memory: bool = True,
+        has_label: bool = True,
+        max_length: Optional[int] = None,
+        text_dtype: tf.DType = tf.int64,
+        label_dtype: tf.DType = tf.int64,
+        column_dtypes=["str", "str", "int"],
+        **kwargs,
+    ):
+        super().__init__(
+            vocab,
+            labels,
+            segmenter=segmenter,
+            X=X,
+            y=y,
+            csv_file=csv_file,
+            in_memory=in_memory,
+            has_label=has_label,
+            max_length=max_length,
+            text_dtype=text_dtype,
+            label_dtype=label_dtype,
+            column_dtypes=column_dtypes,
+            **kwargs,
+        )
+
+    @property
+    def y(self) -> list[int]:
+        if not self.has_label:
+            return []
+        return [int(data[-1]) for data in self._original_dataset.as_numpy_iterator()]
+
+    @property
+    def batch_padding_shapes(self) -> tuple:
+        return [(None, None), ()][: None if self.has_label else -1]
+
+    def py_label_transform(self, label: tf.Tensor) -> tf.Tensor:
+        return label
+
+    def py_transform(self, *data: Sequence[tf.Tensor]) -> list[Any]:
+        token_ids_list = self.vocab.token2idx(
+            self.py_text_transform(data[: -1 if self.has_label else None])
+        )
+        transformed_data = [
+            tf.keras.preprocessing.sequence.pad_sequences(
+                token_ids_list, padding="post"
+            )
+        ]
+        if self.has_label:
+            transformed_data.append(self.py_label_transform(data[-1]))
+        return transformed_data
+
+    def tf_transform_after_py_transform(
+        self, *data: Sequence[tf.Tensor]
+    ) -> Sequence[tf.Tensor]:
+        return data
+
+    def py_transform_out_dtype(self) -> tf.DType:
+        return super(RetrievalDataset, self).py_transform_out_dtype()
+
+    def batchify(
+        self,
+        batch_size: int,
+        shuffle: bool = True,
+        training: bool = True,
+        shuffle_buffer_size: Optional[int] = None,
+        after_batch: Optional[Callable] = None,
+    ) -> tf.data.Dataset:
+        if after_batch is None:
+            after_batch = _reshape_x
+        return super().batchify(
+            batch_size,
+            shuffle=shuffle,
+            training=training,
+            shuffle_buffer_size=shuffle_buffer_size,
+            after_batch=after_batch,
+        )
+
+
 def _bert_generate_y(x):
     reshaped_x = tf.reshape(x, (-1, tf.shape(x)[-1]))
     return (
@@ -109,7 +215,7 @@ class BertRetrievalDataset(BertDatasetMixin, RetrievalDataset):
     def py_transform_out_dtype(self) -> tf.DType:
         return self.text_dtype
 
-    def py_text_transform(self, tokens_tensor: tf.Tensor) -> list[np.ndarray]:
+    def py_text_transform(self, tokens_tensor: tf.Tensor) -> np.ndarray:
         cls_id: int = self.vocab["[CLS]"]
         sep_id: int = self.vocab["[SEP]"]
         pad_id: int = self.vocab[self.vocab.pad]
@@ -126,7 +232,7 @@ class BertRetrievalDataset(BertDatasetMixin, RetrievalDataset):
             token_ids_list, padding="post"
         )
 
-    def py_transform(self, data: tf.Tensor) -> list[Any]:
+    def py_transform(self, data: tf.Tensor) -> np.ndarray:
         return self.py_text_transform(data)
 
     def batchify(
@@ -139,6 +245,62 @@ class BertRetrievalDataset(BertDatasetMixin, RetrievalDataset):
     ) -> tf.data.Dataset:
         if after_batch is None:
             after_batch = _bert_generate_y
+        return super().batchify(
+            batch_size,
+            shuffle=shuffle,
+            training=training,
+            shuffle_buffer_size=shuffle_buffer_size,
+            after_batch=after_batch,
+        )
+
+
+def _bert_reshape_x(*data):
+    x = data[0]
+    reshaped_x = tf.reshape(x, (-1, tf.shape(x)[-1]))
+    type_ids = tf.zeros_like(reshaped_x, dtype=tf.int64)
+    if len(data) == 1:
+        return ((reshaped_x, type_ids),)
+    else:
+        return ((reshaped_x, type_ids), data[-1])
+
+
+class BertRetrievalEvaluationDataset(BertDatasetMixin, RetrievalEvaluationDataset):
+    @property
+    def batch_padding_shapes(self) -> tuple:
+        return [(None, None), ()][: None if self.has_label else -1]
+
+    def py_transform_out_dtype(self) -> list[tf.DType]:
+        return [self.text_dtype, self.label_dtype][: None if self.has_label else -1]
+
+    def py_text_transform(self, tokens_tensor: tf.Tensor) -> list[np.ndarray]:
+        cls_id: int = self.vocab["[CLS]"]
+        sep_id: int = self.vocab["[SEP]"]
+        pad_id: int = self.vocab[self.vocab.pad]
+        token_ids_list = []
+        for _, tokens in enumerate(tokens_tensor.numpy().tolist()):
+            token_ids = self.vocab.token2idx(
+                [token.decode("UTF-8") for token in tokens][: self.max_length]
+            )
+            token_ids = [tid for tid in token_ids if tid != pad_id]
+            token_ids.insert(0, cls_id)
+            token_ids.append(sep_id)
+            token_ids_list.append(token_ids)
+        return [
+            tf.keras.preprocessing.sequence.pad_sequences(
+                token_ids_list, padding="post"
+            )
+        ]
+
+    def batchify(
+        self,
+        batch_size: int,
+        shuffle: bool = True,
+        training: bool = True,
+        shuffle_buffer_size: Optional[int] = None,
+        after_batch: Optional[Callable] = None,
+    ) -> tf.data.Dataset:
+        if after_batch is None:
+            after_batch = _bert_reshape_x
         return super().batchify(
             batch_size,
             shuffle=shuffle,
