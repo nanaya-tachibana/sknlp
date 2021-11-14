4abb03dca8dfe289d44c5f07f4be7ebbef8b2170
nanaya <nanaya100@gmail.com>
2021-11-14 20:08:03 +0800

    test: 删除无效测试

--- tests/layers/test_mlp.py
+++ tests/layers/test_mlp.py
@@ -7,9 +7,9 @@ def test_mlp():
     output_size = 10
     hidden_size = 20
     mlp = MLPLayer(num_layers, hidden_size, output_size)
-    assert len(mlp.dense_layers) == num_layers
-    assert len(mlp.batchnorm_layers) == num_layers - 1
 
     batch_size = 4
     random_data = tf.random.normal((batch_size, 50))
     assert mlp(random_data).shape.as_list() == [4, output_size]
+
+    assert len(mlp.dense_layers) == num_layers
