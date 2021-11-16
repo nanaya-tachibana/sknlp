9038fa153613a2b416e6bcf0babe450a07aa9fd5
nanaya <nanaya100@gmail.com>
2021-11-16 16:27:10 +0800

    test ci

--- .github/scripts/download_pretrain.sh
+++ .github/scripts/download_pretrain.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 mkdir pretrain
-gdown --id 1RC1YsnxlgOsa24A7_BiHimgMJlK-8rYR -O pretrain/albert.tar
-gdown --id 16AvDelZ7U0a0sbsi1rdFEV2cm_0pPlLJ -O pretrain/char.tar
-gdown --id 1o__V6nJ6hh0NWe7NrcXND0nUEJl8kZaP -O pretrain/electra.tar
-gdown --id 1wPcw_9gor7RqekxdbMxEP50L_PppsmnM -O pretrain/bert.tar
+gdown --id 1RC1YsnxlgOsa24A7_BiHimgMJlK-8rYR -O pretrain/albert.tar -q
+gdown --id 16AvDelZ7U0a0sbsi1rdFEV2cm_0pPlLJ -O pretrain/char.tar -q
+gdown --id 1o__V6nJ6hh0NWe7NrcXND0nUEJl8kZaP -O pretrain/electra.tar -q
+gdown --id 1wPcw_9gor7RqekxdbMxEP50L_PppsmnM -O pretrain/bert.tar -q
 for file in pretrain/*.tar
 do 
     tar -xf "$file"  -C pretrain
